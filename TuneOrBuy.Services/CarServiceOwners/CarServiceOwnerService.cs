51a32f400bdf27fb3269ad53ccf2e8542eb460ed
Gerasim Peshev <gerasimpeshev@gmail.com>
2023-07-27 19:40:13 +0300

    Added funcrionality to CarServiceOwnerService

--- TuneOrBuy.Services/CarServiceOwners/CarServiceOwnerService.cs
+++ TuneOrBuy.Services/CarServiceOwners/CarServiceOwnerService.cs
@@ -4,14 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using TuneOrBuy.Data.Models;
 using TuneOrBuy.Services.Contracts;
 using TuneOrBuy.Web.Data;
+using CarServiceOwner = TuneOrBuy.Data.Models.CarServiceOwner;
 
 namespace TuneOrBuy.Services.CarServiceOwners
 {
-    public class CarServiceOwnerService : ICarServiceOwner
+    public class CarServiceOwnerService : ICarServiceOwnerService
     {
         private readonly TuneOrBuyDbContext context;
+        private ICarServiceOwnerService _carServiceOwnerImplementation;
 
         public CarServiceOwnerService(TuneOrBuyDbContext data)
         {
@@ -21,6 +24,34 @@ namespace TuneOrBuy.Services.CarServiceOwners
         public async Task<bool> UserIsCarServiceOwner(Guid userId)
         {
             return await context.CarServiceOwners.AnyAsync(c => c.BuyerId == userId);
+        }
+
+        public async Task<bool> ExistsById(Guid userId)
+        {
+            return await context.CarServiceOwners.AnyAsync(c => c.BuyerId == userId);
+        }
+
+        public async Task<bool> ExistsByPhoneNumber(string phoneNumber)
+        {
+            return await context.CarServiceOwners.AnyAsync(c => c.PhoneNumber == phoneNumber);
+        }
+
+        public async Task CreateSeller(Guid userId, string phoneNumber)
+        {
+            var carServiceOwnerToAdd = new CarServiceOwner()
+            {
+                BuyerId = userId,
+                Buyer = await GetBuyerAsync(userId),
+                PhoneNumber = phoneNumber
+            };
+
+            await context.CarServiceOwners.AddAsync(carServiceOwnerToAdd);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task<Buyer> GetBuyerAsync(Guid userId)
+        {
+            return await context.Users.FirstAsync(u => u.Id == userId);
         }
     }
 }
