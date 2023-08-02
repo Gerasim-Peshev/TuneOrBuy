3508887fd9519d12d6124b44aac9ca2fd521b017
Gerasim Peshev <gerasimpeshev@gmail.com>
2023-08-02 18:15:03 +0300

    Added common class for checkboxes

---
+++ TuneOrBuy.Web/Models/CheckBoxOption.cs
@@ -0,0 +1,9 @@
+﻿namespace TuneOrBuy.Web.Models
+{
+    public class CheckBoxOption
+    {
+        public bool IsChecked { get; set; }
+        public string Description { get; set; } = null!;
+        public string Value { get; set; } = null!;
+    }
+}
