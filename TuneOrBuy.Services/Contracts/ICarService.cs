21a71c4fbfbfb02aa88218b77da16cbb8d32cb0e
Gerasim Peshev <gerasimpeshev@gmail.com>
2023-08-02 18:09:53 +0300

    Added functionality to ICarService and ISellerService and created IPartService

--- TuneOrBuy.Services/Contracts/ICarService.cs
+++ TuneOrBuy.Services/Contracts/ICarService.cs
@@ -5,18 +5,24 @@ namespace TuneOrBuy.Services.Contracts
 {
     public interface ICarService
     {
-        Task<Seller> GetSeller(string userId);
+        Task<Seller> GetSellerByBuyerIdAsync(string userId);
+        Task<Seller> GetSellerBySellerIdAsync(string userId);
+        Task<Buyer> GetBuyerByIdAsync(string userId);
+        Task<Town> GetTownByIdAsync(int townId);
         Task<CarServiceModel> GetCar(string carId);
         Task<List<CarServiceModel>> AllCarsAsync();
-        Task<IEnumerable<CarServiceModel>> MyCarsAsync(string userId);
-        Task CreateCarAsync(string manufacturer, string brand, string bodyType, string vin, string fuel, int horsePower, int year, int firstRegistrationYear, decimal price, int traveledDistance, string sellerId, string imageUrl, string gearType, string color, string numberOfDoors, string numberOfSeats, IEnumerable<string> equipments, string description, bool serviceHistory);
-        Task<CarServiceModel> CarDetailsByIdAsync(string carId);
+        Task CreateCarAsync(string manufacturer, string brand, string bodyType, string vin, string fuel, int horsePower, int year, int firstRegistrationYear, decimal price, int traveledDistance, string sellerId, string imageUrl, string gearType, string color, string numberOfDoors, string numberOfSeats, List<string> equipments, string description, bool serviceHistory);
+        Task<CarDetailsServiceModel> CarDetailsByIdAsync(string carId);
 
         Task EditCarAsync(string id, string manufacturer, string brand, string bodyType, string vin, string fuel, int horsePower,
-                       DateTime year, DateTime firstRegistrationYear, decimal price, int traveledDistance,
+                       int year, int firstRegistrationYear, decimal price, int traveledDistance,
                        string sellerId, string imageUrl, string gearType, string color, string numberOfDoors, string numberOfSeats,
-                       IEnumerable<string> equipments, string description, bool serviceHistory);
+                       string equipments, string description, bool serviceHistory);
 
         Task DeleteCar(string  carId);
+
+        Task<List<CarServiceModel>> MyCarsAsync(string userId);
+        Task ToFavouriteCars(string carId, string userId);
+        Task<Tuple<bool, Buyer, Car>> ContainsCar(string carId, string userId);
     }
 }
--- TuneOrBuy.Services/Contracts/ISellerService.cs
+++ TuneOrBuy.Services/Contracts/ISellerService.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore.Metadata.Conventions;
 using TuneOrBuy.Data.Models;
+using TuneOrBuy.Services.Cars.Models;
+using TuneOrBuy.Services.Parts.Models;
 
 namespace TuneOrBuy.Services.Contracts
 {
@@ -12,11 +14,14 @@ namespace TuneOrBuy.Services.Contracts
     {
         Task<bool> UserIsSeller(Guid userId);
         Task<Seller> GetSeller(string userId);
+
         Task<bool> ExistsById(Guid userId);
         Task<bool> ExistsByPhoneNumber(string phoneNumber);
         Task CreateSeller(Guid userId, string phoneNumber, int townId, string imageUrl);
         Task<IEnumerable<Town>> GetAllTowns();
         Task<Buyer> GetBuyerAsync(Guid userId);
         Task<Town> GetTownByIdAsync(int townId);
+        Task<List<CarServiceModel>> GetAllCarsForSell(string userId);
+        Task<List<PartServiceModel>> GetAllPartsForSell(string userId);
     }
 }
