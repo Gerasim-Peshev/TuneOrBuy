86ea3851c0838bd3db8121354ec8493972978c24
Gerasim Peshev <gerasimpeshev@gmail.com>
2023-08-03 22:38:07 +0300

    Added Name, Description and ImageUrl to Parts and Description to CarService

---
+++ TuneOrBuy.Data/Migrations/20230802155248_AddedNameDescriptionAndImageUrlToPartsAndDescriptionToCarServices.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TuneOrBuy.Data.Migrations
+{
+    public partial class AddedNameDescriptionAndImageUrlToPartsAndDescriptionToCarServices : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Parts",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Parts",
+                type: "nvarchar(2000)",
+                maxLength: 2000,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Parts",
+                type: "nvarchar(2048)",
+                maxLength: 2048,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "CarServices",
+                type: "nvarchar(2000)",
+                maxLength: 2000,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Parts");
+
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Parts");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Parts",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "CarServices",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(2000)",
+                oldMaxLength: 2000,
+                oldNullable: true);
+        }
+    }
+}
