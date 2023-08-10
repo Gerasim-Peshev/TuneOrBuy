f3dc52b5b2233fad1d3f8d3c218a016d5da1d5e6
Gerasim Peshev <gerasimpeshev@gmail.com>
2023-08-10 19:54:58 +0300

    Created admin area

---
+++ TuneOrBuy.Web/Areas/Admin/AdminConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace TuneOrBuy.Web.Areas.Admin
+{
+    public class AdminConstants
+    {
+        public const string AdministratorRoleName = "admin";
+        public const string AdminAreaName = "Admin";
+    }
+}
---
+++ TuneOrBuy.Web/Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TuneOrBuy.Web.Areas.Admin.Controllers
+{
+    [Area(AdminConstants.AdminAreaName)]
+    [Authorize(Roles = AdminConstants.AdministratorRoleName)]
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
